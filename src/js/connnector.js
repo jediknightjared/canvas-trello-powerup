bde0fdc2cd77cf8c45b4736b8486e7cb6305915f
jediknightjared <jaredthebillionaire@outlook.com>
2023-05-18 07:22:37 -0600

    Initialize settings capability

--- src/js/connnector.js
+++ src/js/connnector.js
@@ -2,4 +2,15 @@ var Promise = TrelloPowerUp.Promise;
 
 console.log("Test");
 
-TrelloPowerUp.initialize({});
+TrelloPowerUp.initialize({
+  "show-settings": function (t, options) {
+    // when a user clicks the gear icon by your Power-Up in the Power-Ups menu
+    // what should Trello show. We highly recommend the popup in this case as
+    // it is the least disruptive, and fits in well with the rest of Trello's UX
+    return t.popup({
+      title: "Custom Fields Settings",
+      url: "../html/settings.html",
+      height: 184 // we can always resize later
+    });
+  }
+});
