8980396b823fd92008dd05a617174c458b9ec6f5
jediknightjared <jaredthebillionaire@outlook.com>
2023-05-18 22:06:50 -0600

    Add card-from-url capability

--- src/js/connnector.js
+++ src/js/connnector.js
@@ -4,13 +4,32 @@ console.log("Test");
 
 TrelloPowerUp.initialize({
   "show-settings": function (t, options) {
-    // when a user clicks the gear icon by your Power-Up in the Power-Ups menu
-    // what should Trello show. We highly recommend the popup in this case as
-    // it is the least disruptive, and fits in well with the rest of Trello's UX
     return t.popup({
       title: "Custom Fields Settings",
       url: "/settings.html",
-      height: 184 // we can always resize later
+      height: 184
     });
+  },
+  "card-from-url": function (t, options) {
+    // options.url has the url in question
+    // if we know cool things about that url we can give Trello a name and desc
+    // to use when creating a card. Trello will also automatically add that url
+    // as an attachment to the created card
+    // As always you can return a Promise that resolves to the card details
+
+    const url = options.url;
+
+    console.log(url);
+
+    return new Promise(function (resolve) {
+      resolve({
+        name: "💻 " + options.url + " 🤔",
+        desc: "This Power-Up knows cool things about the attached url"
+      });
+    });
+
+    // if we don't actually have any valuable information about the url
+    // we can let Trello know like so:
+    // throw t.NotHandled();
   }
 });
