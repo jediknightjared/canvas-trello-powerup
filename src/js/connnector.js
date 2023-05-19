760fdfef6ad739f69a72acb31db9cd89ada4a95f
jediknightjared <jaredthebillionaire@outlook.com>
2023-05-18 23:06:19 -0600

    Add Access-Control-Allow-Origin header

--- src/js/connnector.js
+++ src/js/connnector.js
@@ -29,7 +29,12 @@ TrelloPowerUp.initialize({
 
     const fetchURL = `https://${domain}.instructure.com/api/v1/courses/${courseID}/assignments/${assignmentID}?access_token=${token}`;
 
-    const response = await fetch(fetchURL);
+    const response = await fetch(fetchURL, {
+      mode: "cors",
+      headers: {
+        "Access-Control-Allow-Origin": "*"
+      }
+    });
     console.log(response);
     const data = await response.json();
     console.log(data);
